641deb01332e3d79c4e09264a5f81d3a88b555d6
MykolaMazhula <mykm3ua@gmail.com>
2024-05-13 07:27:11 +0200

    Optimize requests with JOIN

--- cinema/views.py
+++ cinema/views.py
@@ -39,6 +39,11 @@ class MovieViewSet(viewsets.ModelViewSet):
             return MovieRetriveSerializer
         return MovieSerializer
 
+    def get_queryset(self):
+        if self.action in ["list", "retrieve"]:
+            return Movie.objects.prefetch_related("actors", "genres")
+        return super().get_queryset()
+
 
 class MovieSessionViewSet(viewsets.ModelViewSet):
     queryset = MovieSession.objects.all()
@@ -49,3 +54,8 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
         elif self.action == "retrieve":
             return MovieSessionRetrieveSerializer
         return MovieSessionSerializer
+
+    def get_queryset(self):
+        if self.action in ["list", "retrieve"]:
+            return MovieSession.objects.select_related("movie", "cinema_hall")
+        return super().get_queryset()
